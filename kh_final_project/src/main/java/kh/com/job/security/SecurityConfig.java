359117da2467eeb0d28fea7952d38547225e8a23
NukNim <tkdtlrdl02@naver.com>
2023-03-07 08:47:10 +0900

    시큐리티 미적용 상태
    시큐리티 미적용으로 인한 서버 실행 오류
    해당 부분 삭제 빈 클래스파일로 생성
    및 한글 가능 테스트

--- kh_final_project/src/main/java/kh/com/job/security/SecurityConfig.java
+++ kh_final_project/src/main/java/kh/com/job/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package kh.com.job.security;
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 
-public class SecurityConfig extends AbstractSecurityWebApplicationInitializer {
+//public class SecurityConfig extends AbstractSecurityWebApplicationInitializer {
+public class SecurityConfig{
 
 }
