4bd7b01df93ae492460e4466689c07c43bbe32b3
NukNim <tkdtlrdl02@naver.com>
2023-03-06 21:19:27 +0900

    spring security jar add
    spring security jar add

--- kh_final_project/src/main/java/kh/com/job/temp/controller/TempController.java
+++ kh_final_project/src/main/java/kh/com/job/temp/controller/TempController.java
@@ -1,14 +1,11 @@
 package kh.com.job.temp.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import kh.com.job.temp.model.dto.TempDto;
 import kh.com.job.temp.model.service.TempService;
 
 @Controller
