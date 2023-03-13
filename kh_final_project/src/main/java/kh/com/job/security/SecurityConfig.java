8c2727b5ad01ce6dc7d3b6c803cf8981899973ce
NukNim <tkdtlrdl02@naver.com>
2023-03-13 16:48:28 +0900

    오늘 진행한 commons 메이븐
    commons-io commons-fileupload 적용
    Securityconfig파일 깡통 만듬

--- kh_final_project/src/main/java/kh/com/job/security/SecurityConfig.java
+++ kh_final_project/src/main/java/kh/com/job/security/SecurityConfig.java
@@ -1,9 +1,5 @@
 package kh.com.job.security;
 
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
-//@EnableWebSecurity
 public class SecurityConfig{
-//public class SecurityConfig{
 
 }
