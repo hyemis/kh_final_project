4b8ca451acec6774629f6bfd6a935ba63c1115e8
KimMilo <tl123612@gmail.com>
2023-03-16 13:38:41 +0900

    @PostMapping("/login") 어노테이션 적용

--- kh_final_project/src/main/java/kh/com/job/temp/controller/TempController.java
+++ kh_final_project/src/main/java/kh/com/job/temp/controller/TempController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
