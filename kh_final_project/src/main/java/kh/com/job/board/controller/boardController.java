713e313de7d5ba2846ccfbddc1e581fd23d14d30
hyemis <hyem.is.1220@gmail.com>
2023-03-17 12:20:10 +0900

    게시판 관련 jsp 구역 분배

--- kh_final_project/src/main/java/kh/com/job/board/controller/boardController.java
+++ kh_final_project/src/main/java/kh/com/job/board/controller/boardController.java
@@ -18,9 +18,39 @@ public class boardController {
 			return mv;
 		}
 		
-		// 게시판 상세 
-			@GetMapping("/detail")
-			public ModelAndView boarddetail(ModelAndView mv) {
+		// 게시판 - 취준방
+			@GetMapping("/careerpre")
+			public ModelAndView careerpre(ModelAndView mv) {
+				return mv;
+		}	
+			
+		// 게시판 - 신입방
+			@GetMapping("/careerfirst")
+			public ModelAndView careerfirst(ModelAndView mv) {
+				return mv;
+			}
+			
+		// 게시판 - 면접방
+			@GetMapping("/interview")
+			public ModelAndView interview(ModelAndView mv) {
+				return mv;
+			}
+
+		// 게시판 - 질문게시판
+			@GetMapping("/qna")
+			public ModelAndView qna(ModelAndView mv) {
+				return mv;
+			}		
+			
+		// 게시판 - 뉴스레터
+			@GetMapping("/companynews")
+			public ModelAndView companynews(ModelAndView mv) {
+				return mv;
+		}
+			
+		// 게시판 - 회사소개 
+			@GetMapping("/companyinfo")
+			public ModelAndView companyinfo(ModelAndView mv) {
 				return mv;
 		}
 		
@@ -34,7 +64,8 @@ public class boardController {
 			@GetMapping("/postinsert")
 			public ModelAndView postinsert(ModelAndView mv) {
 				return mv;
-		
 
 		}
+			
+			
 }
--- kh_final_project/src/main/webapp/WEB-INF/views/common/header.jsp
+++ kh_final_project/src/main/webapp/WEB-INF/views/common/header.jsp
@@ -68,5 +68,5 @@
             </div> --%>
         </div>
         <!-- Header End -->
- -->
+
 </html>
