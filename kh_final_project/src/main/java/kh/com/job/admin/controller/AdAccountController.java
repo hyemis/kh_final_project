ca6ed5fa114dfae0ea39fa463ac9aadfd99623ec
NukNim <tkdtlrdl02@naver.com>
2023-03-21 07:41:21 +0900

    AdMaincontroller 에서 AdAccountController 분리

--- kh_final_project/src/main/java/kh/com/job/admin/controller/AdAccountController.java
+++ kh_final_project/src/main/java/kh/com/job/admin/controller/AdAccountController.java
@@ -1,20 +1,17 @@
 package kh.com.job.admin.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import kh.com.job.admin.model.service.AdService;
+import kh.com.job.person.model.dto.PsUserDto;
 
-@Controller
-@RequestMapping("/admin")
-public class AdMainController {
+@RequestMapping("/admin/account")
+public class AdAccountController {
+	
 	
 	@Autowired
 	BCryptPasswordEncoder passwordEncoder;
@@ -22,25 +19,21 @@ public class AdMainController {
 	@Autowired
 	private AdService service;
 	
-	@GetMapping("/main")
-	public ModelAndView main(ModelAndView mv) {
+	@GetMapping("/manage")
+	public ModelAndView accountmgr(ModelAndView mv, PsUserDto dto) {
+		
+		
+		mv.addObject("accdto", service.accountmgr());
+		
 		return mv;
 	}
 	
-	@GetMapping("/login")
-	public ModelAndView login(ModelAndView mv) {
+	@GetMapping("/create")
+	public ModelAndView create(ModelAndView mv){
+		
+		
+		
 		return mv;
 	}
-	
-	@GetMapping("/logout")
-	public ModelAndView logout(ModelAndView mv) {
-		return mv;
-	}
-	
-	@PostMapping("/loginform")
-	public ModelAndView loginform(ModelAndView mv) {
-		return mv;
-	}
-
 
 }
