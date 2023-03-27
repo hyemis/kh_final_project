4e771856ad7558dcfeb7014d3fc4141bb3a4ea07
NukNim <tkdtlrdl07@gmail.com>
2023-03-27 13:33:46 +0900

    이거 대문자

--- kh_final_project/src/main/java/kh/com/job/common/error/globalExceptionHandler.java
+++ kh_final_project/src/main/java/kh/com/job/common/error/globalExceptionHandler.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
-public class globalExceptionHandler {
+public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(Exception.class)
 	public ModelAndView CustumExceptionHandler(ModelAndView mv, Exception ex) {
