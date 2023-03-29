b923cba2180f6280d538560c2f85d277ac1ead19
NukNim <tkdtlrdl07@gmail.com>
2023-03-29 11:46:59 +0900

    서버 수정db

--- kh_final_project/src/main/java/kh/com/job/business/model/dto/BsRecruitDto.java
+++ kh_final_project/src/main/java/kh/com/job/business/model/dto/BsRecruitDto.java
@@ -14,34 +14,10 @@ import lombok.ToString;
 @Component
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @ToString
 public class BsRecruitDto {
 //BUSINESS_RECRUIT_ANNOUNCEMENT
-	
-	private Number raNum;
-	private String recruitType;
-	private String careerType;
-	private String inchargeWork;
-	private String department;
-	private String raPosition;
-	private String conditionType;
-	private String userEducation;
-	private Timestamp userBirth;
-	private String salary;
-	private String workType;
-	private String workDay;
-	private String workTime;
- 	private String raRepresentativeJob;
-	private String applicationPeriod;
-	private String raType;
-	private String applyType;
-	private String applyFormType;
-	private String raTitle;
-	private String raContent;
-	private String raProcess;
-	private String raExtraDocument;
+
 	
 
 	
