146f0143c5bb504eb5430fb87d11eca4401129e7
Hanwxxl <hanwxxl@gmail.com>
2023-03-30 13:55:05 +0900

    워크넷API-채용공고 DB추가
    30개

--- kh_final_project/src/main/java/kh/com/job/business/model/dao/BsRecruitDao.java
+++ kh_final_project/src/main/java/kh/com/job/business/model/dao/BsRecruitDao.java
@@ -1,8 +1,11 @@
 package kh.com.job.business.model.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import kh.com.job.business.model.dto.BsRecruitDto;
 
@@ -13,8 +16,12 @@ public class BsRecruitDao {
 	private SqlSession sqlSession;
 	
 	// 워크넷 API DB저장
-	public int apiInsert(BsRecruitDto dto) throws Exception {
-		return sqlSession.insert("business.apiInsert", dto);
+	public int apiInsert(List<BsRecruitDto> bsList) throws Exception {
+		int result = 0;
+		for(BsRecruitDto dto : bsList) {
+			result += sqlSession.insert("business.apiInsert", dto);
+		}
+		return result;
 	}
 
 }
