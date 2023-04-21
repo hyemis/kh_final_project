d05f10076ebe6b37f5e68804d43c0ea9c18f8d0e
NukNim <tkdtlrdl07@gmail.com>
2023-04-21 11:12:31 +0900

    기업회원 지원자 페이지 메인 구현 중

--- kh_final_project/src/main/java/kh/com/job/business/model/service/BsApplicantService.java
+++ kh_final_project/src/main/java/kh/com/job/business/model/service/BsApplicantService.java
@@ -2,10 +2,13 @@ package kh.com.job.business.model.service;
 
 import java.util.List;
 
+import kh.com.job.business.model.dto.BsRecruitDto;
 import kh.com.job.business.model.dto.InterviewDto;
 
 public interface BsApplicantService {
 	
 	public List<InterviewDto> interviewList(String userId);
+
+	public List<BsRecruitDto> recruitList(String userId);
 
 }
--- kh_final_project/src/main/java/kh/com/job/business/model/service/BsApplicantServiceImpl.java
+++ kh_final_project/src/main/java/kh/com/job/business/model/service/BsApplicantServiceImpl.java
@@ -2,17 +2,28 @@ package kh.com.job.business.model.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kh.com.job.business.model.dao.BsApplicantDao;
+import kh.com.job.business.model.dto.BsRecruitDto;
 import kh.com.job.business.model.dto.InterviewDto;
 
 @Service
 public class BsApplicantServiceImpl implements BsApplicantService {
+	
+	@Autowired
+	private BsApplicantDao dao;
 
 	@Override
 	public List<InterviewDto> interviewList(String userId) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<BsRecruitDto> recruitList(String userId) {
+		return dao.recruitList(userId);
 	}
 	
 	
